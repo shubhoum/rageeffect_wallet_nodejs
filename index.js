fa24b32f5a1e7c6f90629a0b44f2793889ca43dc
Suvam <53977996+shubhoum@users.noreply.github.com>
2024-06-25 22:32:19 +0545

    new port 3001

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3001;
 const qs = require('qs');
 
 // Define your API key (this should be stored securely, e.g., in an environment variable)
